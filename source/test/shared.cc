5aff1bff2d819a8d318f99ccf5c64ee95539cd42
Joshua Buchanan <62662095+jrbuchanan02@users.noreply.github.com>
2022-04-15 15:11:33 -0500

    more self-testing things

--- source/test/shared.cc
+++ source/test/shared.cc
@@ -25,6 +25,7 @@ int main ( int const argc, char const *const *const argv )
 {
     testMatrixAllocateAndFill ( );
     testMatrixVectorMultiplication ( );
+    testMatrixMatrixMultiplication ( );
 }
 
 void setIndex ( MatrixOfDoubles matrix, size_y r, size_y c, double val )
@@ -139,8 +140,105 @@ void testMatrixVectorMultiplication ( )
     std::cout << "\n";
     std::cout << "Actual  : ";
     for ( std::size_t i = 0; i < 3; i++ ) { std::cout << result [ i ] << " "; }
+    std::cout << "\n";
     deleteMatrixOfDoubles ( mat );
     mat = nullptr;
     delete [] result;
     result = nullptr;
+}
+
+void testMatrixMatrixMultiplication ( )
+{
+    MatrixOfDoubles lhs;
+    MatrixOfTriples rhs, out;
+
+    unsigned long long int doubleSize = 0, tripleSize = 0;
+    sizeofMatrixOfDoubles ( &doubleSize );
+    sizeofMatrixOfTriples ( &tripleSize );
+
+    lhs = std::malloc ( doubleSize );
+    rhs = std::malloc ( tripleSize );
+    out = std::malloc ( tripleSize );
+
+    constructMatrixOfDoubles ( lhs, 3, 3 );
+    constructMatrixOfTriples ( rhs, 3, 3 );
+
+    double      lMatrix [] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    long double rMatrix [] = { 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+    setIndexOfDoubles ( lhs, 0, 0, lMatrix [ 0 ] );
+    setIndexOfDoubles ( lhs, 0, 1, lMatrix [ 1 ] );
+    setIndexOfDoubles ( lhs, 0, 2, lMatrix [ 2 ] );
+    setIndexOfDoubles ( lhs, 1, 0, lMatrix [ 3 ] );
+    setIndexOfDoubles ( lhs, 1, 1, lMatrix [ 4 ] );
+    setIndexOfDoubles ( lhs, 1, 2, lMatrix [ 5 ] );
+    setIndexOfDoubles ( lhs, 2, 0, lMatrix [ 6 ] );
+    setIndexOfDoubles ( lhs, 2, 1, lMatrix [ 7 ] );
+    setIndexOfDoubles ( lhs, 2, 2, lMatrix [ 8 ] );
+
+    setIndexOfTriples ( rhs, 0, 0, rMatrix [ 0 ] );
+    setIndexOfTriples ( rhs, 0, 1, rMatrix [ 1 ] );
+    setIndexOfTriples ( rhs, 0, 2, rMatrix [ 2 ] );
+    setIndexOfTriples ( rhs, 1, 0, rMatrix [ 3 ] );
+    setIndexOfTriples ( rhs, 1, 1, rMatrix [ 4 ] );
+    setIndexOfTriples ( rhs, 1, 2, rMatrix [ 5 ] );
+    setIndexOfTriples ( rhs, 2, 0, rMatrix [ 6 ] );
+    setIndexOfTriples ( rhs, 2, 1, rMatrix [ 7 ] );
+    setIndexOfTriples ( rhs, 2, 2, rMatrix [ 8 ] );
+
+    mulDoublesAndTriples ( out, lhs, rhs );
+
+    long double expected [ 3 ][ 3 ] = {
+            { 1 * 10 + 2 * 13 + 3 * 16,
+              1 * 11 + 2 * 14 + 3 * 17,
+              1 * 12 + 2 * 15 + 3 * 18 },
+            { 4 * 10 + 5 * 13 + 6 * 16,
+              4 * 11 + 5 * 14 + 6 * 17,
+              4 * 12 + 5 * 15 + 6 * 18 },
+            { 7 * 10 + 8 * 13 + 9 * 16,
+              7 * 11 + 8 * 14 + 9 * 17,
+              7 * 12 + 8 * 15 + 9 * 18 },
+    };
+
+    std::cout << "Expected: ";
+    for ( std::size_t r = 0; r < 3; r++ )
+    {
+        // C++ arrays are column-major order, the opposite of ml Matrices.
+        std::cout << expected [ r ][ 0 ] << ", " << expected [ r ][ 1 ] << ", "
+                  << expected [ r ][ 2 ] << "; ";
+    }
+
+    std::cout << "\n";
+    std::cout << "Actual  : ";
+    for ( std::size_t r = 0; r < 3; r++ )
+    {
+        for ( std::size_t c = 0; c < 3; c++ )
+        {
+            long double temp = 0;
+            getIndexOfTriples ( out, r, c, &temp );
+            std::cout << temp;
+            if ( c == 2 )
+            {
+                std::cout << "; ";
+            } else
+            {
+                std::cout << ", ";
+            }
+        }
+    }
+    std::cout << "\n";
+    deleteMatrixOfDoubles ( lhs );
+    deleteMatrixOfTriples ( rhs );
+    deleteMatrixOfTriples ( out );
+    lhs = rhs = out = nullptr;
+}
+
+void testEchelon()
+{
+    // TODO #2 Implement testEchelon
+}
+
+void testInverse()
+{
+    // TODO #3 Implement testInverse
 }
